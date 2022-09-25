d4a30134622169f494b035ac0ee45179b015d830
Andrew-aka-Lemon <105217029+Andrew-aka-Lemon@users.noreply.github.com>
2022-09-25 21:16:22 +0300

    task 2 DONE

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Vimeo from '@vimeo/player';
+// import throttle from 'lodash.throttle';
+import Throttle from 'lodash.throttle';
 
+const VIDEO_CURRENT_TIME = 'videoplayer-current-time';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo(iframe);
+
+setVideoTime();
+// console.log(localStorage.getItem(VIDEO_CURRENT_TIME));
+
+player.on('timeupdate', Throttle(saveCurrentVideoTime, 1000, { leading: false }));
+
+function saveCurrentVideoTime(info) {
+  localStorage.setItem(VIDEO_CURRENT_TIME, info.seconds);
+  console.log(info);
+}
+
+function setVideoTime() {
+  if (!localStorage.getItem(VIDEO_CURRENT_TIME)) {
+    return;
+  }
+  player.setCurrentTime(localStorage.getItem(VIDEO_CURRENT_TIME));
+}
