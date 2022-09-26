638ed0f01864e17a959ee76add0a50e1ad0ae20f
Andrew-aka-Lemon <105217029+Andrew-aka-Lemon@users.noreply.github.com>
2022-09-26 13:25:57 +0300

    task 3 DONE

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,24 +2,24 @@ import Vimeo from '@vimeo/player';
 // import throttle from 'lodash.throttle';
 import Throttle from 'lodash.throttle';
 
-const VIDEO_CURRENT_TIME = 'videoplayer-current-time';
+const VIDEO_CURRENT_TIME_KEY = 'videoplayer-current-time';
 
 const iframe = document.querySelector('#vimeo-player');
 const player = new Vimeo(iframe);
 
 setVideoTime();
-// console.log(localStorage.getItem(VIDEO_CURRENT_TIME));
+// console.log(localStorage.getItem(VIDEO_CURRENT_TIME_KEY));
 
 player.on('timeupdate', Throttle(saveCurrentVideoTime, 1000, { leading: false }));
 
 function saveCurrentVideoTime(info) {
-  localStorage.setItem(VIDEO_CURRENT_TIME, info.seconds);
-  console.log(info);
+  localStorage.setItem(VIDEO_CURRENT_TIME_KEY, info.seconds);
+  // console.log(info);
 }
 
 function setVideoTime() {
-  if (!localStorage.getItem(VIDEO_CURRENT_TIME)) {
+  if (!localStorage.getItem(VIDEO_CURRENT_TIME_KEY)) {
     return;
   }
-  player.setCurrentTime(localStorage.getItem(VIDEO_CURRENT_TIME));
+  player.setCurrentTime(localStorage.getItem(VIDEO_CURRENT_TIME_KEY));
 }
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import throttle from 'lodash.throttle';
 
+import { saveToLocalStorage, loadFromLocalStorage, removeFromLocalStorage } from './jsonService';
+
+const formRef = document.querySelector('.feedback-form');
+
+const USER_CURRENT_INPUT_KEY = 'feedback-form-state';
+
+let currentFormData = {};
+
+if (loadFromLocalStorage(USER_CURRENT_INPUT_KEY)) {
+  currentFormData = loadFromLocalStorage(USER_CURRENT_INPUT_KEY);
+}
+
+setFormData();
+
+formRef.addEventListener('input', throttle(onFormInput, 500));
+// formRef.addEventListener('input', onFormInput);
+formRef.addEventListener('submit', throttle(onFormSubmit, 500));
+
+function onFormInput(event) {
+  const { name, value } = event.target;
+  currentFormData[name] = value;
+
+  saveToLocalStorage(USER_CURRENT_INPUT_KEY, currentFormData);
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const { email, message } = event.currentTarget.elements;
+
+  const filledFormData = { email: email.value, message: message.value };
+  console.log(filledFormData);
+  formRef.reset();
+  removeFromLocalStorage(USER_CURRENT_INPUT_KEY);
+}
+
+function setFormData() {
+  if (!loadFromLocalStorage(USER_CURRENT_INPUT_KEY)) {
+    return;
+  }
+
+  const parcedData = loadFromLocalStorage(USER_CURRENT_INPUT_KEY);
+  Object.keys(parcedData).forEach(element => {
+    formRef.elements[element].value = parcedData[element];
+  });
+}
